821a9c8ed237d338b2c4ae5124eb252ac6c59874
DELL <urvisha.sohamtech@gmail.com>
2023-06-05 05:54:24 -0700

    add

--- lib/add student.dart
+++ lib/add student.dart
@@ -15,25 +15,34 @@ class _AddStudentState extends State<AddStudent> {
   final TextEditingController emailcontroller = TextEditingController();
   final TextEditingController mobilecontroller = TextEditingController();
 
-  final formKey = GlobalKey<FormState>();
   List<Map<String, dynamic>> myData = [];
+  bool isLoading =false;
 
-  void refreshData() async{
-    final data = await DatabaseHelper.getItems();
-    setState(() {
-      myData = data;
-    });
-  }
+  // void refreshData() async{
+  //   final data = await DatabaseHelper.getItems();
+  //   setState(() {
+  //     myData = data;
+  //     isLoading = false;
+  //   });
+  // }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   // refreshData();
+  // }
 
   Future<void> addItem() async {
-    await DatabaseHelper.insertItem(
-        idcontroller.text, namecontroller.text, dobcontroller.text, emailcontroller.text, mobilecontroller.text);
-    refreshData();
-  }
-  Future<void> updateItem() async{
-    await DatabaseHelper.updateItem(
-        idcontroller.text, namecontroller.text, dobcontroller.text, emailcontroller.text, mobilecontroller.text);
-    refreshData();
+    try {
+      await DatabaseHelper.insertItem(
+          idcontroller.text,
+          namecontroller.text,
+          dobcontroller.text,
+          emailcontroller.text,
+          mobilecontroller.text);
+      // refreshData();
+    }catch( error){
+      debugPrint('*** error');
+    }
   }
 
   @override
@@ -46,102 +55,100 @@ class _AddStudentState extends State<AddStudent> {
       body:SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.all(16),
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                const Text(
-                  'Add Details',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                  ),
+          child: Column(
+            children: [
+              const Text(
+                'Add Details',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
                 ),
-                const SizedBox(
-                  height: 20,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: idcontroller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter ID',
+                  labelText: 'ID',
                 ),
-                TextField(
-                  controller: idcontroller,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter ID',
-                    labelText: 'ID',
-                  ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: namecontroller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Name',
+                  labelText: 'Name',
                 ),
-                const SizedBox(
-                  height: 10,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: dobcontroller,
+                decoration:const  InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Dob',
+                  labelText: 'Date Of Birth',
                 ),
-                TextField(
-                  controller: namecontroller,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter Name',
-                    labelText: 'Name',
-                  ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: emailcontroller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Email',
+                  labelText: 'Email',
                 ),
-                const SizedBox(
-                  height: 10,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: mobilecontroller,
+                decoration:const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter No',
+                  labelText: 'Mobile no.',
                 ),
-                TextField(
-                  controller: dobcontroller,
-                  decoration:const  InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter Dob',
-                    labelText: 'Date Of Birth',
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  controller: emailcontroller,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter Email',
-                    labelText: 'Email',
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  controller: mobilecontroller,
-                  decoration:const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter No',
-                    labelText: 'Mobile no.',
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        primary: Colors.white,
-                        textStyle: TextStyle(fontSize: 15)
-                      ),
-                      child: Text('Add Detail'),
-                        onPressed: (){
-                        DatabaseHelper.insertItem(
-                            idcontroller.text,
-                            namecontroller.text,
-                            dobcontroller.text,
-                            emailcontroller.text,
-                            mobilecontroller.text);
-                        DatabaseHelper.getItems();
-                          Navigator.pop(context);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text('New Student Add')));
-                         },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      primary: Colors.white,
+                      textStyle: TextStyle(fontSize: 15)
                     ),
-                  ],
-                ),
-              ],
-            ),
+                    child: Text('Add Detail'),
+                      onPressed: (){
+                      setState(() {
+                        try {
+                          addItem();
+                          DatabaseHelper.getItems();
+                        }catch(error){
+                          debugPrint('** error');
+                        }
+                      });
+                        Navigator.pop(context);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('New Student Add')));
+                       },
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- lib/database.dart
+++ lib/database.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
@@ -13,6 +14,7 @@ class DatabaseHelper {
   }
 
   static Future<void> createTable(database) async {
+    try {
       database.execute("""CREATE TABLE Student(
         id INTEGER PRIMARY KEY,
         name TEXT,
@@ -21,18 +23,31 @@ class DatabaseHelper {
         mobile INTEGER 
         )
         """);
+    }catch(error){
+      debugPrint('** error');
+    }
     }
 
   static Future<int> insertItem(id, name, dob, email, mobile )async{
-    final db = await DatabaseHelper.db();
-    final data ={'id': id,'name': name,'dob': dob,'email': email,'mobile':mobile};
-    final result =  db.insert('Student',data,conflictAlgorithm: ConflictAlgorithm.replace);
-    return result;
+    try {
+      final db = await DatabaseHelper.db();
+      final data = {'id': id, 'name': name, 'dob': dob, 'email': email, 'mobile': mobile};
+      final result = db.insert('Student', data, conflictAlgorithm: ConflictAlgorithm.replace);
+      return result;
+    }catch(error){
+      debugPrint('** error');
+      return 0;
+    }
   }
 
-  static Future<List<Map<String,dynamic>>> getItems() async{
-    final db = await DatabaseHelper.db();
-    return db.query('Student',orderBy: 'id');
+  static Future<Object> getItems() async{
+    try {
+      final db = await DatabaseHelper.db();
+      return db.query('Student');
+    }catch(error){
+      debugPrint('** error');
+      return 0;
+    }
   }
 
   static Future<int> updateItem( id,name,dob,email,mobile)async{
